664f688616323770bd09cb59fe3d3dcbc5e945fb
Pku.画师 <44673428+mjx990307@users.noreply.github.com>
2026-05-21 10:52:24 +0800

    add common leetcode java practice problems

---
+++ leetcode/704_binary_search.java
@@ -0,0 +1,22 @@
+/**
+ * LeetCode 704. Binary Search
+ * Time: O(log n), Space: O(1)
+ */
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
